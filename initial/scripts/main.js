2c838f69d6dc1843383f441d34a8265b2a128482
chenxingyu <1019209240@qq.com>
2021-11-01 10:39:15 +0800

    first html

---
+++ initial/scripts/main.js
@@ -0,0 +1,15 @@
+let a = 2;
+let b = 2
+let c = !(a !== b)
+let myHeading = document.querySelector('h1');
+myHeading.textContent = "hello world!";
+
+function multiply(num1, num2) {
+    return a + b
+}
+alert(c)
+alert(multiply(a, b))
+
+document.querySelector('h1').onclick = function () {
+    alert('别戳我，我怕疼。');
+}
