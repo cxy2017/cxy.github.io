378f372f473f29c0f6459c713ffcc4eca9613074
chenxingyu <1019209240@qq.com>
2021-11-01 15:57:05 +0800

    add title switch

--- initial/scripts/main.js
+++ initial/scripts/main.js
@@ -2,14 +2,18 @@ let a = 2;
 let b = 2
 let c = !(a !== b)
 let myHeading = document.querySelector('h1');
-myHeading.textContent = "hello world!";
-
+myHeading.textContent = "hello my js world!";
 function multiply(num1, num2) {
     return a + b
 }
-alert(c)
-alert(multiply(a, b))
+
 
 document.querySelector('h1').onclick = function () {
     alert('别戳我，我怕疼。');
+    let myHeadTitle = myHeading.textContent 
+    if(myHeadTitle==='hello my js world!'){
+        myHeading.textContent = "欢迎来到我的HTML";
+    }else{
+        myHeading.textContent = "hello my js world!";
+    }
 }
