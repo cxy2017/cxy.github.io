239ae0df827a59c3227f5ad240c59ff2dcd8ad5b
cxy <1019209240@qq.com>
2021-11-01 21:43:04 +0800

    fix

--- initial/scripts/main.js
+++ initial/scripts/main.js
@@ -1,30 +1,33 @@
-let a = 2;
-let b = 2
-let c = !(a !== b)
+
 let myHeading = document.querySelector('h1');
-myHeading.textContent = "hello my js world!";
-function multiply(num1, num2) {
-    return a + b
-}
-
-
-document.querySelector('h1').onclick = function () {
-    alert('别戳我，我怕疼。');
-    let myHeadTitle = myHeading.textContent 
-    if(myHeadTitle==='hello my js world!'){
-        myHeading.textContent = "欢迎来到我的HTML";
-    }else{
-        myHeading.textContent = "hello my js world!";
+let myButton = document.querySelector('button');
+myHeading.onclick = function () {
+    if (myHeading.textContent === "切换666") {
+        myHeading.textContent = "hello world!";
+    } else if (myHeading.textContent === "hello world!") {
+        myHeading.textContent = "明天会更好";
+    } else {
+        myHeading.textContent = "切换666";
     }
 }
 
 let myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'initial/images/1.jpg') {
-      myImage.setAttribute('src', 'initial/images/2.png');
+myImage.onclick = function () {
+    let scr = myImage.getAttribute("src");
+    if (scr === "initial/images/1.jpg") {
+        myImage.setAttribute("src", "initial/images/2.png")
     } else {
-      myImage.setAttribute('src', 'initial/images/1.jpg');
+        myImage.setAttribute("src", "initial/images/1.jpg")
+    }
+}
+
+myButton.onclick = setUserName();
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if (!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = '欢迎您的到来，' + myName;
     }
 }
